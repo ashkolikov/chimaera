020665b3ce56cb8e235f863a79c776035a723c94
Aleksandra Galitsyna <agalitzina@gmail.com>
2021-10-27 09:36:07 -0400

    Project restructure as a package. 1. library files moved to "chimaera" folder 2. data files movel to separate folder 3. environment.yml and requirements.txt files added; README updated 4. setup.py added and tested 5. test folder created (no tests yet) 6. no change to notebooks

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 ![image](https://user-images.githubusercontent.com/79586312/127057849-d2785b2e-8f5b-4daf-bc44-ad3dbc3fd47e.png)
 Hi-C is one of the most popular methods for studying the spatial organization of the genome. The result of the experiment and subsequent processing of the sequencing data is a map that shows the frequency of contacts of genome regions with each other. This model can predict such maps from a raw DNA sequence.
 ![Poster MCCMB](https://user-images.githubusercontent.com/79586312/138556876-739fa8c5-3939-4a50-abdd-12316b66c9e0.png)
+
+### Installation with conda
+
+Pull the project:
+```bash
+git clone https://github.com/ashkolikov/chimaera.git
+cd chimaera
+```
+
+Create environment:
+```bash
+conda env create -f environment.yml
+conda activate chimaera
+```
+
+Install (dev mode):
+```bash
+pip install -e .
+```
+
+Run tests: 
+```bash
+pip install -e .
+```
+
+Enable Jupyter support: 
+```bash
+python -m ipykernel install --user --name chimaera --display-name "chimaera"
+```
---
+++ setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup
+import re
+import os
+import io
+
+
+def _read(*parts, **kwargs):
+    filepath = os.path.join(os.path.dirname(__file__), *parts)
+    encoding = kwargs.pop("encoding", "utf-8")
+    with io.open(filepath, encoding=encoding) as fh:
+        text = fh.read()
+    return text
+
+
+def get_version():
+    version = re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+        _read("chimaera", "__init__.py"),
+        re.MULTILINE,
+    ).group(1)
+    return version
+
+
+def get_requirements(path):
+    content = _read(path)
+    return [
+        req
+        for req in content.split("\n")
+        if req != "" and not (req.startswith("#") or req.startswith("-"))
+    ]
+
+setup_requires = [
+    'cython',
+    'numpy',
+]
+install_requires = get_requirements("requirements.txt")
+
+setup(
+    name="chimaera",
+    version=get_version(),
+    author="ashkolikov",
+    license="MIT",
+    py_modules=["chimaera"],
+    setup_requires=setup_requires,
+    install_requires=install_requires,
+)
