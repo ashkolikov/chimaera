8fe9e986f9af19ca5526bf3851e4c7c383de092f
ashkolikov <79586312+ashkolikov@users.noreply.github.com>
2023-06-14 23:16:03 +0300

    Update model.py

--- model.py
+++ model.py
@@ -1,14 +1,11 @@
-import os
-import numpy as np
-import matplotlib.pyplot as plt
-from tensorflow.keras.utils import to_categorical, Sequence
 import tensorflow as tf
 import tensorflow.keras.backend as K
-import keras_nlp
+#import keras_nlp
 
 class MainModel():
     def __init__(self,
                  data,
+                 decoder,
                  latent_dim = 64,
                  filters = [64, 128, 256, 512, 64],
                  kernel_sizes = [19, 9, 5, 3, 3],
@@ -24,141 +21,93 @@ class MainModel():
                  residual_block_dropout_rates = [0, 0],
                  attention_block_dropout_rate = 0.1):
 
-        self.data = data
-        self.latent_dim = latent_dim
 
-        self.filters = filters
-        self.kernel_sizes = kernel_sizes
-        self.dilation_rates = dilation_rates
-        self.n_residual_blocks = n_residual_blocks
-        self.n_attention_blocks = n_attention_blocks
-        self.residual_block_filters = residual_block_filters
-        self.residual_block_kernel_sizes = residual_block_kernel_sizes
-        self.residual_block_dilation_rate_factors = residual_block_dilation_rate_factors
-        self.querry_dim = querry_dim
-        self.n_heads = n_heads
-        self.dropout_rates = dropout_rates
-        self.residual_block_dropout_rates = residual_block_dropout_rates
-        self.attention_block_dropout_rate = attention_block_dropout_rate
+        inp = tf.keras.Input((data.dna_len, 4))
 
-        self.input = tf.keras.Input((self.data.dna_len, 4))
-        self.output, self.attention_outputs = self.build_model()
-
-    def build_model(self):
-        x = tf.keras.layers.Conv1D(self.filters[0],
-                                   self.kernel_sizes[0],
-                                   dilation_rate=self.dilation_rates[0],
-                                   padding='same',
-                                   kernel_initializer='he_normal')(self.input)
+        x = tf.keras.layers.Conv1D(filters[0], kernel_sizes[0],  dilation_rate=dilation_rates[0],  padding='same', kernel_initializer='he_normal')(inp)
         x = tf.keras.layers.BatchNormalization()(x)
         x = tf.keras.layers.ReLU()(x)
         x = tf.keras.layers.MaxPooling1D(pool_size=2)(x)
-        x = tf.keras.layers.Dropout(self.dropout_rates[0])(x)
+        x = tf.keras.layers.Dropout(dropout_rates[0])(x)
 
-        pooling_range = int(np.log2(self.data.dna_len)) - 8
+        pooling_range = int(np.log2(data.dna_len)) - 8
         for block in range(pooling_range-2):
-            x = tf.keras.layers.Conv1D(self.filters[1],
-                                       self.kernel_sizes[1],
-                                       dilation_rate=self.dilation_rates[1],
-                                       padding='same',
-                                       kernel_initializer='he_normal')(x)
+            x = tf.keras.layers.Conv1D(filters[1], kernel_sizes[1], dilation_rate=dilation_rates[1], padding='same', kernel_initializer='he_normal')(x)
             x = tf.keras.layers.BatchNormalization()(x)
             x = tf.keras.layers.ReLU()(x)
             x = tf.keras.layers.MaxPooling1D(pool_size=2)(x)
-            x = tf.keras.layers.Dropout(self.dropout_rates[1])(x)
+            x = tf.keras.layers.Dropout(dropout_rates[1])(x)
 
-        x = tf.keras.layers.Conv1D(self.filters[1],
-                                   self.kernel_sizes[1],
-                                   dilation_rate=self.dilation_rates[1],
-                                   padding='same',
-                                   kernel_initializer='he_normal')(x)
+        x = tf.keras.layers.Conv1D(filters[1], kernel_sizes[1], dilation_rate=dilation_rates[1], padding='same', kernel_initializer='he_normal')(x)
         x = tf.keras.layers.BatchNormalization()(x)
         x = tf.keras.layers.ReLU()(x)
 
         x_ = x
-        for block in range(self.n_residual_blocks):
-            x = tf.keras.layers.Conv1D(self.residual_block_filters[0],
-                                       self.residual_block_kernel_sizes[0],
-                                       padding='same',
-                                       dilation_rate=self.residual_block_dilation_rate_factors[0]*(block+1),
-                                       kernel_initializer='he_normal')(x)
+        for block in range(n_residual_blocks):
+            x = tf.keras.layers.Conv1D(residual_block_filters[0], residual_block_kernel_sizes[0], padding='same', dilation_rate=residual_block_dilation_rate_factors[0]*(block+1), kernel_initializer='he_normal')(x)
             x = tf.keras.layers.BatchNormalization()(x)
             x = tf.keras.layers.ReLU()(x)
-            x = tf.keras.layers.Dropout(self.residual_block_dropout_rates[0])(x)
+            x = tf.keras.layers.Dropout(residual_block_dropout_rates[0])(x)
 
-            x = tf.keras.layers.Conv1D(self.residual_block_filters[1],
-                                       self.residual_block_kernel_sizes[1],
-                                       padding='same',
-                                       dilation_rate=self.residual_block_dilation_rate_factors[1]*(block+1),
-                                       kernel_initializer='he_normal')(x)
+            x = tf.keras.layers.Conv1D(residual_block_filters[1], residual_block_kernel_sizes[1], padding='same', dilation_rate=residual_block_dilation_rate_factors[1]*(block+1), kernel_initializer='he_normal')(x)
             x = tf.keras.layers.BatchNormalization()(x)
-            x = tf.keras.layers.Dropout(self.residual_block_dropout_rates[1])(x)
+            x = tf.keras.layers.Dropout(residual_block_dropout_rates[1])(x)
             x = tf.keras.layers.Add()([x_, x])
             x = tf.keras.layers.ReLU()(x)
             x_ = x
         
         x_ = x
         atts = []
-        for i in range(self.n_attention_blocks):
+        for i in range(n_attention_blocks):
             x = x + keras_nlp.layers.SinePositionEncoding()(x)
-            x, att = tf.keras.layers.MultiHeadAttention(self.n_heads,
-                                                        self.querry_dim)(x, x, return_attention_scores=True)
+            x, att = tf.keras.layers.MultiHeadAttention(n_heads, querry_dim)(x, x, return_attention_scores=True)
             atts.append(att)
             x = tf.keras.layers.BatchNormalization()(x)
-            x = tf.keras.layers.Dropout(self.attention_block_dropout_rate)(x)
+            x = tf.keras.layers.Dropout(attention_block_dropout_rate)(x)
             
             x = tf.keras.layers.Add()([x_, x])
             x_ = x
             x = tf.keras.layers.ReLU()(x)
 
 
-        x = tf.keras.layers.Conv1D(self.filters[2],
-                                   self.kernel_sizes[2],
-                                   dilation_rate=self.dilation_rates[2],
-                                   padding='same',
-                                   kernel_initializer='he_normal')(x)
+        x = tf.keras.layers.Conv1D(filters[2], kernel_sizes[2], dilation_rate=dilation_rates[2], padding='same', kernel_initializer='he_normal')(x)
         x = tf.keras.layers.BatchNormalization()(x)
         x = tf.keras.layers.ReLU()(x)
-        x = tf.keras.layers.Dropout(self.dropout_rates[2])(x)
-        x = tf.keras.layers.Conv1D(self.filters[3],
-                                   self.kernel_sizes[3],
-                                   dilation_rate=self.dilation_rates[3],
-                                   padding='same',
-                                   kernel_initializer='he_normal')(x)
+        x = tf.keras.layers.Dropout(dropout_rates[2])(x)
+        x = tf.keras.layers.Conv1D(filters[3], kernel_sizes[3], dilation_rate=dilation_rates[3], padding='same', kernel_initializer='he_normal')(x)
         x = tf.keras.layers.BatchNormalization()(x)
         x = tf.keras.layers.ReLU()(x)
         x = tf.keras.layers.MaxPooling1D(pool_size=2)(x)
-        x = tf.keras.layers.Dropout(self.dropout_rates[3])(x)
+        x = tf.keras.layers.Dropout(dropout_rates[3])(x)
 
-        x = tf.keras.layers.Conv1D(self.filters[4],
-                                   self.kernel_sizes[4],
-                                   dilation_rate=self.dilation_rates[4],
-                                   padding='same',
-                                   kernel_initializer='he_normal')(x)
+        x = tf.keras.layers.Conv1D(filters[4], kernel_sizes[4], dilation_rate=dilation_rates[4], padding='same', kernel_initializer='he_normal')(x)
         x = tf.keras.layers.BatchNormalization()(x)
         x = tf.keras.layers.ReLU()(x)
         x = tf.keras.layers.MaxPooling1D(pool_size=2)(x)
-        x = tf.keras.layers.Dropout(self.dropout_rates[4])(x)
+        x = tf.keras.layers.Dropout(dropout_rates[4])(x)
         x = tf.keras.layers.Flatten()(x)
-        x = tf.keras.layers.Dense(self.latent_dim,
-                                  kernel_initializer='he_normal')(x)
+        x = tf.keras.layers.Dense(latent_dim, kernel_initializer='he_normal')(x)
+        x = decoder(x)
+        for k,v in decoder._get_trainable_state().items():
+            k.trainable = False
 
-        output = x
+        model = tf.keras.Model(inp, x)
         att_models = []
         for att in atts:
-            att_models.append(tf.keras.Model(self.inputinp, att))
-
-        return output, att_models
+            att_models.append(tf.keras.Model(inp, att))
+        self.input = inp
+        self.output = x
+        self.model = model
+        self.attention_outputs = att_models
         
 
 class VAE():
     def __init__(self,
                  data,
                  latent_dim=64):
+        self.latent_dim = latent_dim
         h = data.y_val[0].shape[1]
         w = data.y_val[0].shape[2]
-        self.latent_dim = latent_dim
-        self.data = data
 
         inputs = tf.keras.Input(shape=(h, w, 1))
         x = tf.keras.layers.Conv2D(16, 5, activation="relu", strides=1, padding="same")(inputs)
@@ -191,19 +140,13 @@ class VAE():
         x = tf.keras.layers.Conv2DTranspose(32, 3, activation="relu", strides=1, padding="same")(x)
         x = tf.keras.layers.BatchNormalization()(x)
         x = tf.keras.layers.Conv2DTranspose(32, 1, activation="relu", strides=1, padding="same")(x)
-        if data.normalize == 'standart':
-            outputs = tf.keras.layers.Dense(1)(x)
-        else:
-            outputs = tf.keras.layers.Dense(1, activation='sigmoid')(x)
+        outputs = tf.keras.layers.Dense(1)(x)
         decoder = tf.keras.Model(latent_inputs, outputs, name='decoder')
 
         outputs = decoder(encoder(inputs)[2])
         vae = tf.keras.Model(inputs, outputs, name='vae')
         
-        if data.normalize == 'standart':
-            reconstruction_loss = tf.keras.losses.mse(inputs, outputs)
-        else:
-            reconstruction_loss = tf.keras.losses.binary_crossentropy(inputs, outputs)
+        reconstruction_loss = tf.keras.losses.mse(inputs, outputs)
         reconstruction_loss *= h * w
         reconstruction_loss = K.mean(reconstruction_loss)
         kl_loss = 1 + z_log_sigma - K.square(z_mean) - K.exp(z_log_sigma)
@@ -226,7 +169,7 @@ class VAE():
         return z_mean + K.exp(z_log_sigma) * epsilon
 
 
-class CompositeModel():
+class Chimaera():
     def __init__(self,
                  data,
                  model_dir=None,
@@ -244,8 +187,10 @@ class CompositeModel():
                  dropout_rates = [0, 0.1, 0.15, 0.15, 0.15],
                  residual_block_dropout_rates = [0, 0],
                  attention_block_dropout_rate = 0.1):
-        
-        custom_objects = {'SinePositionEncoding': keras_nlp.layers.SinePositionEncoding}
+        try:
+            custom_objects = {'SinePositionEncoding': keras_nlp.layers.SinePositionEncoding}
+        except:
+            custom_objects = {}
         self.model_1d = None
         self.data = data
 
@@ -259,7 +204,13 @@ class CompositeModel():
             self.attention_outputs = []
             self.latent_model = None
         else:
+            vae = VAE(data=data, latent_dim=latent_dim)
+            self.encoder = vae.encoder
+            self.decoder = vae.decoder
+            self.ae = vae.vae
+
             main_model = MainModel(data = data,
+                                   decoder = self.decoder,
                                     latent_dim = latent_dim,
                                     filters = filters,
                                     kernel_sizes = kernel_sizes,
@@ -278,13 +229,5 @@ class CompositeModel():
             latent_output = main_model.output
             self.attention_outputs = main_model.attention_outputs
             self.latent_model = tf.keras.Model(inp, latent_output)
-
-            vae = VAE(data=data, latent_dim=latent_dim)
-            self.encoder = vae.encoder
-            self.decoder = vae.decoder
-            self.ae = vae.vae
-
-            final_output = self.decoder(latent_output)
-            self.model = tf.keras.Model(inp, final_output)
-            self.model.compile(optimizer='adam', loss='mse')  
-            
+            self.model = self.latent_model
+            self.model.compile(optimizer='adam', loss='mse')
