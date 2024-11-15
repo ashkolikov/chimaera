28739c52606a2eba2e934c057144ab8480a5d285
ashkolikov <79586312+ashkolikov@users.noreply.github.com>
2024-11-15 19:40:51 +0300

    Update __init__.py

--- chimaera/__init__.py
+++ chimaera/__init__.py
@@ -1,2 +1,2 @@
 __version__ = "0.0"
-from . import plot_utils, preprocess, data_utils, dataset, models, train_utils, trainer, latent, motifs, ism, gradients, genes, chimaera
+from . import plot_utils, preprocess, data_utils, dataset, models, train_utils, trainer, latent, motifs, ism, gradients, genes, chimaera, crosspred, benchmark
